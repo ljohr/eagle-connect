38b87af9dc33457489ec35ef4b6f563e13a721cb
Lilly Ohr <lillyohr@gmail.com>
2023-12-03 23:34:29 -0500

    Editing styling

--- client/src/pages/Registration/Register.jsx
+++ client/src/pages/Registration/Register.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import LogoLink from "../../assets/LogoBC.png"
+import LogoLink from "../../assets/LogoBC.png";
 import "./Register.css";
 
 const Register = () => {
   return (
-    <body>
-      <div class="bclogo">
+    <div className="registration-container">
+      <div className="bclogo">
         <img src={LogoLink} width="480" height="270"></img>
       </div>
-      <main>
+      <main className="register-main">
         <h2>Select Your Identity</h2>
         <button>
           <Link to="/mentor/register">Register as a Mentor</Link>
@@ -17,7 +17,7 @@ const Register = () => {
           <Link to="/student/register">Register as a Student</Link>
         </button>
       </main>
-    </body>
+    </div>
   );
 };
 
