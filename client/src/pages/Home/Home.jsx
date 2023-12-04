8d56662838791d63a34bd40e1a01815609029eae
Lilly Ohr <lillyohr@gmail.com>
2023-12-04 00:49:23 -0500

    Editing styling

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,7 +10,6 @@ const Hero = () => {
 
       <h3>Your Ultimate BC Networking Companion </h3>
       <div className="home-login-container">
-
         <button>
           <Link to="/mentor/login">Login as a Mentor</Link>
         </button>
@@ -24,9 +23,10 @@ const Hero = () => {
 
 const Info = () => {
   return (
-    <div className="info">
+    <div className="home-info">
       <p>
-      Explore careers or offer help on our platform designed for meaningful connections in our vibrant community.
+        Explore careers or offer help on our platform designed for meaningful
+        connections in our vibrant community.
       </p>
     </div>
   );
@@ -35,30 +35,38 @@ const Info = () => {
 const KeyImage = () => {
   return (
     <div class="image-container">
-  <div class="image-wrapper">
-    <div class="circle-box">
-      <img src="https://www.svgrepo.com/show/492701/supporting-business-person.svg" alt="Image 1"></img>
-    </div>
-    <p class="image-caption">Connect to experienced alumni</p>
-  </div>
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/492701/supporting-business-person.svg"
+            alt="Image 1"
+          ></img>
+        </div>
+        <p class="image-caption">Connect to experienced alumni</p>
+      </div>
 
-  <div class="image-wrapper">
-    <div class="circle-box">
-      <img src="https://www.svgrepo.com/show/530239/schedule.svg" alt="Image 2"></img>
-    </div>
-    <p class="image-caption">Book Meetings with Our Scheduling Tool</p>
-  </div>
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/530239/schedule.svg"
+            alt="Image 2"
+          ></img>
+        </div>
+        <p class="image-caption">Book Meetings with Our Scheduling Tool</p>
+      </div>
 
-  <div class="image-wrapper">
-    <div class="circle-box">
-      <img src="https://www.svgrepo.com/show/493366/meeting-person.svg" alt="Image 3"></img>
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/493366/meeting-person.svg"
+            alt="Image 3"
+          ></img>
+        </div>
+        <p class="image-caption">Discover Your Preferred Method of Meeting</p>
+      </div>
     </div>
-    <p class="image-caption">Discover Your Preferred Method of Meeting</p>
-  </div>
-</div>
   );
 };
-
 
 const HowToJoin = () => {
   return (
@@ -67,35 +75,50 @@ const HowToJoin = () => {
       <ol>
         <li>
           <strong>Sign Up:</strong>
-          <p>Create your Eagle Connect account in minutes. Provide details about your academic background, interests, and career goals to enhance your profile.</p>
+          <p>
+            Create your Eagle Connect account in minutes. Provide details about
+            your academic background, interests, and career goals to enhance
+            your profile.
+          </p>
         </li>
 
         <li>
           <strong>Discover Profiles:</strong>
-          <p>Explore profiles of students and alumni who share your professional interests. Our matching algorithm connects you with potential mentors, mentees, or networking partners.</p>
+          <p>
+            Explore profiles of students and alumni who share your professional
+            interests. Our matching algorithm connects you with potential
+            mentors, mentees, or networking partners.
+          </p>
         </li>
 
         <li>
           <strong>Schedule Meetings:</strong>
-          <p>Use our scheduling tool to find convenient meeting times. Whether it's a virtual Zoom meeting or an in-person coffee chat, networking has never been this easy.</p>
+          <p>
+            Use our scheduling tool to find convenient meeting times. Whether
+            it's a virtual Zoom meeting or an in-person coffee chat, networking
+            has never been this easy.
+          </p>
         </li>
 
         <li>
           <strong>Request Mentorship:</strong>
-          <p>Reach out to experienced alumni by sending mentorship requests. Receive guidance, insights, and advice to navigate your academic and professional journey.</p>
+          <p>
+            Reach out to experienced alumni by sending mentorship requests.
+            Receive guidance, insights, and advice to navigate your academic and
+            professional journey.
+          </p>
         </li>
       </ol>
     </div>
   );
 };
 
-
 const Home = () => {
   return (
     <main>
       <Hero />
       <Info />
-      <KeyImage/>
+      <KeyImage />
       <HowToJoin />
     </main>
   );
