8fe32866a50a4e1823e2cd5893cf58b8732e2d7a
Lilly Ohr <lillyohr@gmail.com>
2023-12-03 20:52:38 -0500

    Added dashboard data

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,3 +1,5 @@
+import profileLink1 from "../../assets/profile-1.svg";
+import profileLink2 from "../../assets/profile-2.svg";
 import "./Dashboard.css";
 
 const Dashboard = () => {
@@ -12,12 +14,41 @@ const Dashboard = () => {
               <h4 className="time">December 20th at 4pm</h4>
               <div className="appointment-inner">
                 <div className="profile-img">
-                  <img src="../assets/profile-1.svg" alt="profile-image" />
+                  <img src={profileLink1} alt="profile-image" />
                 </div>
                 <div className="appointment-info">
                   <p className="name">James Smith</p>
                   <p>Class of 2020</p>
                   <p>Software Engineer working at Datadog</p>
+                  <p className="btn-category">Coffee Chat</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="appointment-single">
+              <h4 className="time">December 27th at 4pm</h4>
+              <div className="appointment-inner">
+                <div className="profile-img">
+                  <img src={profileLink1} alt="profile-image" />
+                </div>
+                <div className="appointment-info">
+                  <p className="name">James Smith</p>
+                  <p>Class of 2020</p>
+                  <p>Software Engineer working at Datadog</p>
+                  <p className="btn-category">Resume Review</p>
+                </div>
+              </div>
+            </div>
+            <div className="appointment-single">
+              <h4 className="time">January 10th at 1pm</h4>
+              <div className="appointment-inner">
+                <div className="profile-img">
+                  <img src={profileLink2} alt="profile-image" />
+                </div>
+                <div className="appointment-info">
+                  <p className="name">Audrey Shin</p>
+                  <p>Class of 2022</p>
+                  <p>Software Engineer working at MongoDB</p>
                   <p className="btn-category">Coffee Chat</p>
                 </div>
               </div>
@@ -28,7 +59,7 @@ const Dashboard = () => {
             <div className="mentor-single">
               <div className="mentor-inner">
                 <div className="profile-img">
-                  <img src="../assets/profile-1.svg" alt="profile-image" />
+                  <img src={profileLink1} alt="profile-image" />
                 </div>
                 <div className="appointment-info">
                   <p className="name">Dan Alejo</p>
@@ -40,12 +71,51 @@ const Dashboard = () => {
                 </div>
               </div>
             </div>
+
+            <div className="mentor-single">
+              <div className="mentor-inner">
+                <div className="profile-img">
+                  <img src={profileLink2} alt="profile-image" />
+                </div>
+                <div className="appointment-info">
+                  <p className="name">Katie Gupta</p>
+                  <p>Class of 2021</p>
+                  <p>Software Engineer working at Apple</p>
+                  <p className="btn-category">Resume Review</p>
+                  <p className="btn-category">Zoom</p>
+                </div>
+              </div>
+            </div>
+            <div className="mentor-single">
+              <div className="mentor-inner">
+                <div className="profile-img">
+                  <img src={profileLink1} alt="profile-image" />
+                </div>
+                <div className="appointment-info">
+                  <p className="name">John Wilson</p>
+                  <p>Class of 2020</p>
+                  <p>Software Engineer working at Bloomberg</p>
+                  <p className="btn-category">Coffee Chat</p>
+                  <p className="btn-category">Zoom</p>
+                </div>
+              </div>
+            </div>
+            <div className="mentor-single">
+              <div className="mentor-inner">
+                <div className="profile-img">
+                  <img src={profileLink2} alt="profile-image" />
+                </div>
+                <div className="appointment-info">
+                  <p className="name">Jane Lim</p>
+                  <p>Class of 2016</p>
+                  <p>Software Engineer working at Audible</p>
+                  <p className="btn-category">Coffee Chat</p>
+                  <p className="btn-category">Zoom</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-      </div>
-
-      <div>
-        <h2>Saved Mentors</h2>
       </div>
     </main>
   );
