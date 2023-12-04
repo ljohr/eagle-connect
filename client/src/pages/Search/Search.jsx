99a09e3365b3d36183c5afed4b3d96e1b70cf29d
Danni Qu <angelaqu129@gmail.com>
2023-12-03 19:49:39 -0500

    merge branches

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,10 +1,8 @@
 import { useContext } from "react";
 import { AuthContext } from "../AuthContext";
-<<<<<<< HEAD
 import Search_student from "./Search_student.jsx";
 import Search_mentor from "./Search_mentor.jsx";
-=======
->>>>>>> lilly_branch
+
 
 const Search = () => {
   const { isStudent } = useContext(AuthContext);
