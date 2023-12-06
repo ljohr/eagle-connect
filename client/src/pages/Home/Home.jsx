a413eb47657c35754a669c83fc9278186c74b71b
Danni Qu <angelaqu129@gmail.com>
2023-12-06 11:41:49 -0500

    home page change

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -68,50 +68,105 @@ const KeyImage = () => {
   );
 };
 
-const HowToJoin = () => {
+const HowToJoinTitle = () => {
   return (
     <div className="how-to-join">
-      <h3>How To Join</h3>
-      <ol>
-        <li>
-          <strong>Sign Up:</strong>
-          <p>
-            Create your Eagle Connect account in minutes. Provide details about
-            your academic background, interests, and career goals to enhance
-            your profile.
-          </p>
-        </li>
+      <h3>Connect in 4 Steps</h3>
+      </div>
+  )
+}
 
-        <li>
-          <strong>Discover Profiles:</strong>
-          <p>
-            Explore profiles of students and alumni who share your professional
-            interests. Our matching algorithm connects you with potential
-            mentors, mentees, or networking partners.
-          </p>
-        </li>
 
-        <li>
-          <strong>Schedule Meetings:</strong>
-          <p>
-            Use our scheduling tool to find convenient meeting times. Whether
-            it's a virtual Zoom meeting or an in-person coffee chat, networking
-            has never been this easy.
-          </p>
-        </li>
+const HowToJoin = () => {
+  return (
+    <div class="image-container">
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/302839/sign-up-application.svg"
+            alt="Image 1"
+          ></img>
+        </div>
+        <p class="image-caption">Sign Up</p>
+      </div>
 
-        <li>
-          <strong>Request Mentorship:</strong>
-          <p>
-            Reach out to experienced alumni by sending mentorship requests.
-            Receive guidance, insights, and advice to navigate your academic and
-            professional journey.
-          </p>
-        </li>
-      </ol>
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/302837/productive-person-thinking.svg"
+            alt="Image 2"
+          ></img>
+        </div>
+        <p class="image-caption">Discover Profiles</p>
+      </div>
+
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/302842/stay-active.svg"
+            alt="Image 3"
+          ></img>
+        </div>
+        <p class="image-caption">Schedule Meetings</p>
+      </div>
+      
+      <div class="image-wrapper">
+        <div class="circle-box">
+          <img
+            src="https://www.svgrepo.com/show/302836/new-team-member.svg"
+            alt="Image 4"
+          ></img>
+        </div>
+        <p class="image-caption">Meet Your Mentor</p>
+      </div>
     </div>
   );
 };
+
+// const HowToJoin = () => {
+//   return (
+//     <div className="how-to-join">
+//       <h3>How To Join</h3>
+//       <ol>
+//         <li>
+//           <strong>Sign Up:</strong>
+//           <p>
+//             Create your Eagle Connect account in minutes. Provide details about
+//             your academic background, interests, and career goals to enhance
+//             your profile.
+//           </p>
+//         </li>
+
+//         <li>
+//           <strong>Discover Profiles:</strong>
+//           <p>
+//             Explore profiles of students and alumni who share your professional
+//             interests. Our matching algorithm connects you with potential
+//             mentors, mentees, or networking partners.
+//           </p>
+//         </li>
+
+//         <li>
+//           <strong>Schedule Meetings:</strong>
+//           <p>
+//             Use our scheduling tool to find convenient meeting times. Whether
+//             it's a virtual Zoom meeting or an in-person coffee chat, networking
+//             has never been this easy.
+//           </p>
+//         </li>
+
+//         <li>
+//           <strong>Request Mentorship:</strong>
+//           <p>
+//             Reach out to experienced alumni by sending mentorship requests.
+//             Receive guidance, insights, and advice to navigate your academic and
+//             professional journey.
+//           </p>
+//         </li>
+//       </ol>
+//     </div>
+//   );
+// };
 
 const Home = () => {
   return (
@@ -119,6 +174,7 @@ const Home = () => {
       <Hero />
       <Info />
       <KeyImage />
+      <HowToJoinTitle/>
       <HowToJoin />
     </main>
   );
