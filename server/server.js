5c9ae274b09281b6d9615dc5f6e7f6223c5178ef
Lilly Ohr <lillyohr@gmail.com>
2023-12-08 14:29:08 -0500

    Fixing routes in deployed website

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(
   cors({
