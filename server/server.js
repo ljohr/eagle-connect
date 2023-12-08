4d8f1efc32a895fe296c212fc10e58a81e783ea6
Lilly Ohr <lillyohr@gmail.com>
2023-12-08 14:33:26 -0500

    Fixing routes in deployed website

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
 import connectDB from "./config/connectDB.js";
 import StudentModel from "./models/Student.js";
 import MentorModel from "./models/Mentor.js";
